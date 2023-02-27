c1df279853693942649882fd785c2ee2748de990
Rodion <mrDalmatov@MacBook-Pro-Rodion.local>
2023-02-27 17:00:35 +0700

    all tests is passed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) {
+    return [];
+  }
+
+  let result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let columnIdx = i % 2 === 0 ? j : (matrix[i].length - j - 1);
+      result.push(matrix[i][columnIdx]);
+    }
+  }
+
+  return result;
 }
